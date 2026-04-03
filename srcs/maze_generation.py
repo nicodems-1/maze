3e1257d1f49aadd5c5adf9d52bf40a20b8802ca0
kaze-kc <fontainecharlie2@gmail.com>
2026-04-03 17:21:30 +0200

    added fully working maze generator!

--- srcs/maze_generation.py
+++ srcs/maze_generation.py
@@ -23,44 +23,75 @@ class MazeGenerator:
         self.generate_maze()
         self.output = self.format_output()
 
-
     def generate_maze(self) -> None:
-        current = Cell(randint(0, self.width -1),
-                       randint(0, self.height -1))
+        self.put_42()
+        starting_cell = self.maze[randint(0, self.width)][randint(0, self.height)]
+        self.dfs_algorithm(starting_cell)
+
+    def put_42(self) -> None:
+        x = int(self.width / 2) - 3
+        y = int(self.height / 2) - 3
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            x += 1
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            y += 1
+        for i in range(0, 3):
+            self.maze[x][y].visited = True
+            x += 1
+        x -= 1
+        y += 4
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            y -= 1
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            x -= 1
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            y += 1
+        for i in range(0, 2):
+            self.maze[x][y].visited = True
+            x -= 1
+        for i in range(0, 3):
+            self.maze[x][y].visited = True
+            y -= 1
+
+    def dfs_algorithm(self, current: Cell) -> None:
+        current.visited = True
         while self.unvisited_neighbours(current):
-            current.visited = True
             unv = self.unvisited_neighbours(current)
-            print(len(unv))
             chosen = unv[randint(0, len(unv) - 1)]
             self.break_wall(current, chosen)
-            current = chosen
+            self.dfs_algorithm(chosen)
 
     def unvisited_neighbours(self, current: Cell) -> list[Cell]:
         unvisited_neighbours = []
         x = current.x
         y = current.y
-        if x > 0 and not self.maze[x - 1][y].visited:
+        if x > 0 and self.maze[x - 1][y].visited == False:
             unvisited_neighbours.append(self.maze[x - 1][y])
-        if y > 0 and not self.maze[x][y - 1].visited:
+        if y > 0 and self.maze[x][y - 1].visited == False:
             unvisited_neighbours.append(self.maze[x][y - 1])
-        if x < self.width - 1 and not self.maze[x + 1][y].visited:
+        if x < self.width - 1 and self.maze[x + 1][y].visited == False:
             unvisited_neighbours.append(self.maze[x + 1][y])
-        if y < self.height - 1 and not self.maze[x][y + 1].visited:
+        if y < self.height - 1 and self.maze[x][y + 1].visited == False:
             unvisited_neighbours.append(self.maze[x][y + 1])
         return unvisited_neighbours
 
     @staticmethod
     def break_wall(c1: Cell, c2: Cell) -> None:
-        if c1.y > c2.y:
+        if c1.x > c2.x:
             c1.value &= 0b1110
             c2.value &= 0b1011
-        if c1.x < c2.x:
+        if c1.y < c2.y:
             c1.value &= 0b1101
             c2.value &= 0b0111
-        if c1.y < c2.y:
+        if c1.x < c2.x:
             c1.value &= 0b1011
             c2.value &= 0b1110
-        if c1.x > c2.x:
+        if c1.y > c2.y:
             c1.value &= 0b0111
             c2.value &= 0b1101
 
