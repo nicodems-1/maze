68dea048912431cd5ffe791f63ba26d3582a8d7e
niverdie <niverdie@DESKTOP-A88FCJC.localdomain>
2026-04-27 15:12:01 +0200

    better placement of the menu

--- srcs/visual.py
+++ srcs/visual.py
@@ -116,41 +116,42 @@ class Visual:
         self.fill_path(x, y, (255, 0, 0))
         self.fill_path(j, q, (0, 255, 0))
         self.mlx_instance.mlx_clear_window(self.mlx_ptr, self.win_ptr)
+        logo_placement = 50
         self.mlx_instance.mlx_string_put(
-            self.mlx_ptr, self.win_ptr, 3000, 250, int("0000FF", 16), "ENTRY"
+            self.mlx_ptr, self.win_ptr, logo_placement, 50, int("0000FF", 16), "ENTRY"
         )
         self.mlx_instance.mlx_string_put(
-            self.mlx_ptr, self.win_ptr, 3000, 270, int("00FF00", 16), "EXIT"
+            self.mlx_ptr, self.win_ptr, logo_placement, 70, int("00FF00", 16), "EXIT"
         )
         self.mlx_instance.mlx_string_put(
             self.mlx_ptr,
             self.win_ptr,
-            3000,
-            290,
+            logo_placement,
+            90,
             int("FFE100", 16),
             "C : Change color",
         )
         self.mlx_instance.mlx_string_put(
             self.mlx_ptr,
             self.win_ptr,
-            3000,
-            310,
+            logo_placement,
+            110,
             int("FFE100", 16),
             "Q : Close window",
         )
         self.mlx_instance.mlx_string_put(
             self.mlx_ptr,
             self.win_ptr,
-            3000,
-            330,
+            logo_placement,
+            130,
             int("FFE100", 16),
             "S : Show/hide path",
         )
         self.mlx_instance.mlx_string_put(
             self.mlx_ptr,
             self.win_ptr,
-            3000,
-            350,
+            logo_placement,
+            150,
             int("FFE100", 16),
             "R : Regenerate Maze",
         )
